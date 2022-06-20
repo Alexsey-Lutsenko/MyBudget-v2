cfb39ccef6b1eec903ac1689bf14edff84c24d82
Alexsey Lutsenko <formerfor84qwedf@gmail.com>
2022-06-20 09:09:47 +0700

    setup laravel+vue3

---
+++ resources/js/app.js
@@ -0,0 +1,9 @@
+
+require('./bootstrap');
+
+import {createApp} from 'vue'
+import App from "./views/App";
+
+const app = createApp(App)
+
+app.mount('#app')
---
+++ resources/js/views/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container-fluid">
+        <div class="d-flex justify-content-center">
+            <h1>Hello MyBudget</h1>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "App"
+}
+</script>
+
+<style scoped>
+
+</style>
